7425f1fb962ba974c1e8de7c33430886bbe3fdd4
Bruno Arouca <brunoarouca1@gmail.com>
2023-06-09 13:23:23 -0300

    Feito a componetização do Sidebar, Content e Header do Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
-export function Content() {
-  // Complete aqui
+import { useState, useEffect } from "react";
+import { MovieProps, GenreResponseProps } from "../types/DataProps";
+import { MovieCard } from "./MovieCard";
+import { Header } from "./Header";
+
+interface ContentProps{
+  movies: MovieProps[],
+  selectedGenre: GenreResponseProps; 
+}
+
+export function Content({movies, selectedGenre}: ContentProps) {
+
+  return (
+    <div className="container">
+      
+        <Header title={selectedGenre.title} />
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  );
 }
